57b388349595c313d49b3c7b2a43bd1dda0bd666
yago <benitezdemiguel@gmail.com>
2017-06-06 13:42:10 +0200

    añadir publicacion 1

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.views.generic import RedirectView
 
 from . import views
 
+publications = [
+    url(r'^$', views.PublicationList.as_view(), name='publication-list'),
+    url(r'^new/$', views.PublicationCreate.as_view(), name='publication-create'),
+    url(r'^(?P<pk>[^/]+)/$', views.PublicationDetail.as_view(), name='publication-detail'),
+]
+
+
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='home/')),
     url(r'^home/$', views.Home.as_view(), name='home'),
+    url(r'^publications/', include(publications)),
+
 ]
