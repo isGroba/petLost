309b2fb45bc89bbc83d36585b939f5e422942d38
Ivan Souto <isgroba@gmail.com>
2017-06-07 14:12:05 +0200

    agrega modelos a admin, edita titulo

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,39 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Pet, Publication, Color, Member
+
+
+class PublicationAdmin(admin.ModelAdmin):
+    fields = ['title', 'description', 'pet', 'location']
+    search_fields = ['title', 'breed']
+
+    list_display = ('title', 'description', 'pet', 'date', 'location')
+    list_filter = ['date', 'location']
+
+
+class PetAdmin(admin.ModelAdmin):
+    fields = ['name', 'type_animal', 'breed', 'description', 'color']
+    search_fields = ['name', 'breed']
+
+    list_display = ('name', 'type_animal', 'breed', 'description')
+    list_filter = ['type_animal', 'breed']
+
+
+class ColorAdmin(admin.ModelAdmin):
+    fields = ['name', 'code_color']
+    search_fields = ['name']
+
+    list_display = ('name', 'code_color')
+
+
+class MemberAdmin(admin.ModelAdmin):
+    fields = ['name', 'mail', 'phone', 'city']
+    search_fields = ['name', 'city']
+
+    list_display = ('name', 'mail', 'phone', 'city')
+
+
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Member, MemberAdmin)
